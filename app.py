56f243204515990ef1844bc01e6d6fe67dd8c10c
Marius Bolstad <marius.bolstad@visma.com>
2023-06-26 11:30:53 +0200

    GPT-3.5 chat completion

--- app.py
+++ app.py
@@ -10,26 +10,19 @@ openai.api_key = os.getenv("OPENAI_API_KEY")
 @app.route("/", methods=("GET", "POST"))
 def index():
     if request.method == "POST":
-        animal = request.form["animal"]
-        response = openai.Completion.create(
-            model="text-davinci-003",
-            prompt=generate_prompt(animal),
-            temperature=0.6,
+        input = request.form["chat-input"]
+        response = openai.ChatCompletion.create(
+            model="gpt-3.5-turbo",
+             messages= [
+                            {"role": "user", "content": generate_prompt(input=input)},
+                        ],
+            temperature=0.6
         )
-        return redirect(url_for("index", result=response.choices[0].text))
+        return redirect(url_for("index", result=response.choices[0].message.content))
 
     result = request.args.get("result")
     return render_template("index.html", result=result)
 
 
-def generate_prompt(animal):
-    return """Suggest three names for an animal that is a superhero.
-
-Animal: Cat
-Names: Captain Sharpclaw, Agent Fluffball, The Incredible Feline
-Animal: Dog
-Names: Ruff the Protector, Wonder Canine, Sir Barks-a-Lot
-Animal: {}
-Names:""".format(
-        animal.capitalize()
-    )
+def generate_prompt(input):
+    return "Answer this question using your finest language. Keep it short and concise.   " + input
